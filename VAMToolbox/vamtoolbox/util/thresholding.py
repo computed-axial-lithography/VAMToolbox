04c797a6309faf7086400ce7d886bd6a6d02dad1
Wendy Lin <wendy10168@gmail.com>
2025-12-05 10:04:07 -0800

    Fix NumPy deprecation: replace np.bool with bool for NumPy 1.20+ compatibility

--- VAMToolbox/vamtoolbox/util/thresholding.py
+++ VAMToolbox/vamtoolbox/util/thresholding.py
@@ -2,5 +2,5 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def threshold(x: np.ndarray, thresh: float) -> NDArray[np.bool]:
-    return (x >= thresh).astype(np.bool)
+def threshold(x: np.ndarray, thresh: float) -> NDArray[bool]:
+    return (x >= thresh).astype(bool)
